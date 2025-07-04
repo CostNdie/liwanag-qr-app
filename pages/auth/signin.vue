fed94d5e99d206fc61bd41f3c4f7de22de4ad3fb
joshua <liwanagjoshua82@gmail.com>
2025-07-04 18:11:24 +0800

    added nuxt auth plugins in

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="d-flex align-center justify-center" style="height: 100vh;">
 
-    <v-card
-      class="mx-auto pa-12 pb-8"
-      elevation="8"
-      max-width="448"
-      rounded="lg"
-    >
-      <div class="text-subtitle-1 text-medium-emphasis justify-center">Login Page</div>
+    <v-card width="500" class="pa-8">
+    <h1 class="text-center my-6">Login Page</h1>
+      <div class="text-subtitle-1 text-medium-emphasis justify-center"></div>
 
       <v-text-field
         density="compact"
-        placeholder="Email address"
+        placeholder="Username"
         prepend-inner-icon="mdi-email-outline"
-        variant="outlined"
+        outlined
+        rounded
+        
       ></v-text-field>
 
       <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-        Password
+        
 
         <a
           class="text-caption text-decoration-none text-blue"
@@ -32,14 +30,15 @@
         :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
         :type="visible ? 'text' : 'password'"
         density="compact"
-        placeholder="Enter your password"
+        placeholder="Password"
         prepend-inner-icon="mdi-lock-outline"
-        variant="outlined"
+        outlined
+        rounded
         @click:append-inner="visible = !visible"
       ></v-text-field>
 
       <v-card
-        class="mb-12"
+        class="mb-8"
         color="surface-variant"
         variant="tonal"
       >
@@ -52,10 +51,12 @@
         size="large"
         variant="tonal"
         block
+        rounded="xl"
       >
         Log In
       </v-btn>
-
+      <div class="text-center my-6">OR</div>
+      <v-btn color="error" rounded="xl" class="text-capitalize" block><v-icon left>mdi-google</v-icon> Sign in With google</v-btn>
       <v-card-text class="text-center">
         <a
           class="text-blue text-decoration-none"
